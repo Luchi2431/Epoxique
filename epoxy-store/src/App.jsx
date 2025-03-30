6fcfa306f61078954e93207ded6210dac409751c
Luchi <vlatkovicluka01@gmail.com>
2025-03-30 23:53:39 +0200

    Updated all and ready for deployment

--- epoxy-store/src/App.jsx
+++ epoxy-store/src/App.jsx
@@ -4,10 +4,13 @@ import Footer from './components/Footer/Footer';
 import "./App.css";
 import CollectionItems from './pages/Collection/CollectionItems';
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
-import {Suspence,lazy} from 'react';
+import { Suspense, lazy } from 'react'; // Fixed Suspence to Suspense
 import { CartProvider } from "./context/CartContext";
 import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
+// Lazy imports remain the same
 const Home = lazy(() => import('./pages/Home/Home'));
 const Collection = lazy(() => import('./pages/Collection/Collection'));
 const Checkout = lazy(() => import('./pages/Checkout/Checkout'));
@@ -19,38 +22,38 @@ const Dimensions = lazy(() => import('./pages/Info/Dimensions/Dimensions'));
 const Materials = lazy(() => import('./pages/Info/Materials/Materials'));
 const TableConfigurator = lazy(() => import('./pages/TableConfigurator/TableConfigurator'));
 const ProductDetails = lazy(() => import('./components/ProductDetails.jsx/ProductDetails'));
-const App = () => {
 
+const App = () => {
   return (
     <ErrorBoundary>
-    <CartProvider>
-    <Router>
-      <ScrollToTop />
-      <Navbar/>
-      <div className='main-content'>
-      <Suspense fallback={<div className="loading">Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/checkout" element={<Checkout/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/info/about" element={<About/>}/>
-          <Route path="/collection" element={<Collection />} />
-          <Route path="/collection/:id" element={<CollectionItems />} />
-          <Route path="/info/reviews" element={<Reviews />} />
-          <Route path="/info/warranty" element={<Warranty />} />
-          <Route path="/info/dimensions" element={<Dimensions />} />
-          <Route path="/info/core" element={<Materials />} />
-          <Route path="/tableconfigurator" element={<TableConfigurator/>}/>
-          <Route path="/product/:id" element={<ProductDetails/>}/>
-        </Routes>
-      </Suspense>
-      </div>
-      <Footer />
-    </Router>
-    </CartProvider>
+      <CartProvider>
+        <Router>
+          <ScrollToTop />
+          <Navbar/>
+          <div className='main-content'>
+            <Suspense fallback={<div className="loading">Loading...</div>}>
+              <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/checkout" element={<Checkout/>}/>
+                <Route path="/contact" element={<Contact/>}/>
+                <Route path="/info/about" element={<About/>}/>
+                <Route path="/collection" element={<Collection />} />
+                <Route path="/collection/:id" element={<CollectionItems />} />
+                <Route path="/info/reviews" element={<Reviews />} />
+                <Route path="/info/warranty" element={<Warranty />} />
+                <Route path="/info/dimensions" element={<Dimensions />} />
+                <Route path="/info/core" element={<Materials />} />
+                <Route path="/tableconfigurator" element={<TableConfigurator/>}/>
+                <Route path="/product/:id" element={<ProductDetails/>}/>
+              </Routes>
+            </Suspense>
+          </div>
+          <Footer />
+          <ToastContainer />
+        </Router>
+      </CartProvider>
     </ErrorBoundary>
   );
 }
 
-export default App
-
+export default App;
