3b3abb1c089e4cde412953ffc03c707ffb9cf2ce
Luchi <vlatkovicluka01@gmail.com>
2025-03-31 22:04:16 +0200

    Got categories and products for category id

---
+++ epoxy-store-backend/src/routes/categoryRoutes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+const categoryController = require('../controllers/categoryController');
+
+// Get all categories
+router.get('/', categoryController.getAllCategories);
+
+//Get products by Category ID
+router.get('/:id/products',categoryController.getProductsByCategoryId);
+
+module.exports = router;
