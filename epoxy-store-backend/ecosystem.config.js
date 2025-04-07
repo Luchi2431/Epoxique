a22eb7028582a0afc7fd1e10f798a732267819fa
Luchi <vlatkovicluka01@gmail.com>
2025-04-07 21:06:57 +0200

    update

--- epoxy-store-backend/ecosystem.config.js
+++ epoxy-store-backend/ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   apps: [{
-    name: "epoxy-backend",
-    script: "./src/app.js",
-    watch: false,
-    max_memory_restart: "1G",
-    exec_mode: "fork",
+    name: 'epoxy-backend',
+    script: './src/app.js',
+    exec_mode: 'fork',
     instances: 1,
     autorestart: true,
-    max_restarts: 10,
+    watch: false,
+    max_memory_restart: '1G',
     env: {
-      NODE_ENV: "production",
+      NODE_ENV: 'production',
       PORT: 5000
     }
   }]
