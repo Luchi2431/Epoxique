ff54b3dbc75f38b47b3f2ec25d497881d0f0684f
Luchi <vlatkovicluka01@gmail.com>
2025-04-28 17:07:03 +0200

    Update app.js

--- epoxy-store-backend/src/app.js
+++ epoxy-store-backend/src/app.js
@@ -9,7 +9,14 @@ const orderRoutes = require('./routes/orderRoutes');
 const app = express();
 
 // Middlewares
-app.use(cors());
+app.use(cors({
+    origin: [
+        'https://epoxique.vercel.app',
+        'http://localhost:5173'
+    ],
+    credentials: true,
+    methods: ['GET','POST','PUT','DELETE','OPTIONS']
+}));
 app.use(express.json());
 app.use('/api/images', express.static(path.join(__dirname, '../public/images/products')));
 
