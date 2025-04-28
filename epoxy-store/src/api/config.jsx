cdc20b2aebf5e2ce36b0c4f4c6812d694495d1f7
Luchi <vlatkovicluka01@gmail.com>
2025-04-28 15:22:22 +0200

    new update

--- epoxy-store/src/api/config.jsx
+++ epoxy-store/src/api/config.jsx
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-    BASE_URL: import.meta.env.VITE_API_URL,
+    BASE_URL: import.meta.env.VITE_API_URL || 'https://api/epoxique.rs',
     HEADERS: {
         'Content-Type': 'application/json',
     },
