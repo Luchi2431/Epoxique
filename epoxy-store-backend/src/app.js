2d1a3f0881c322e24f39120f26a826471f85b96f
Luchi <vlatkovicluka01@gmail.com>
2025-04-29 09:45:28 +0200

    update

--- epoxy-store-backend/src/app.js
+++ epoxy-store-backend/src/app.js
@@ -12,11 +12,12 @@ const app = express();
 app.use(cors({
     origin: [
         'https://www.epoxique.rs',
-        'https://epoxique.rs',
+        'https://api.epoxique.rs',
         'http://localhost:5173',
     ],
     credentials: true,
-    methods: ['GET','POST','PUT','DELETE','OPTIONS']
+    methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+    allowedHeaders: ['Content-Type','Authorization']
 }));
 app.use(express.json());
 app.use('/api/images', express.static(path.join(__dirname, '../public/images/products')));
