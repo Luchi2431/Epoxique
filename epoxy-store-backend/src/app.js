e39aa6c60ea837c21f4259759a58c2b2446b922e
Luka Vlatkovic <vlatkovicluka01@gmail.com>
2025-05-17 18:22:27 +0200

    Update app.js

--- epoxy-store-backend/src/app.js
+++ epoxy-store-backend/src/app.js
@@ -14,7 +14,7 @@ app.use(cors({
         'https://www.epoxique.rs',
         'https://api.epoxique.rs',
         'http://localhost:5173',
-        'https://epoxique.rs/api',
+        'https://epoxique.rs',
     ],
     credentials: true,
     methods: ['GET','POST','PUT','DELETE','OPTIONS'],
